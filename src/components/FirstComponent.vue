7058e8ae2cafb63fc9e5652b65b3b36ea6d91d7f
BerengerH <berenger.humeau@yahoo.fr>
2022-07-30 13:39:03 +0200

    First test with vue

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="test-div">
+    <h1>This is a test - Hello World</h1>
+    <p>Just to see, "{{testMessage}}"</p>
+    <a :href="ironhackLink" target="_blank">{{testLinkMessage}}</a>
+    <input type="text" v-model="textInput"> <span>Here is your text input: "{{textInput}}""</span>
+    <button @click="btnClicked(); helloWorld();">Test btn for v-on</button>
+  </div>
+</template>
+
+<script>
+/* Mostachos
+  v-bind (:)
+  v-on (@)
+  v-model */
+export default {
+  data() {
+    return {
+        testMessage: "let's try out some string templates!",
+        testLinkMessage: "Here is some text to try dynamic links with v-bind.",
+        ironhackLink: "https://www.ironhack.com",
+        textInput: "",
+    };
+  },
+  methods: {
+    btnClicked(){
+        alert("The button was correctly clicked");
+    },
+    helloWorld(){
+        console.log("Hello World");
+    },
+  },
+}
+</script>
+
+<style scoped>
+.test-div{
+    background-color: #edede9;
+    margin: 80px auto;
+}
+
+.test-div *{
+    display: block;
+    margin: 20px auto;
+}
+</style>
